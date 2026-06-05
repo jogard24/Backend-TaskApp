d217be46de611e347bf6acc929991a34f7b0ad79
wendy vera <wendyvanesavera@gmail.com>
2026-06-04 22:42:47 -0500

    Refator: Se cambiaron las rutas estaticas de los usuarios por rutas conectadas al controlador

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,24 +4,45 @@ const express = require("express");
 // De express nos traemos la funcion de router
 const users = express.Router();
 
-// .get es quien hace la solicitud a traves del navegador
-users.get('/', function (req, res) {
-  res.json({
-    "mensaje":"Se listaran todos los Ususarios",
-    "nombre": "javid",
-    "años" : 2026
- })
-})
+// // .get es quien hace la solicitud a traves del navegador
+// users.get('/', function (req, res) {
+//   res.json({
+//     "mensaje":"Se listaran todos los Ususarios",
+//     "nombre": "javid",
+//     "años" : 2026
+//  })
+// })
 
-// .post es la funcion quien nos ayuda a agregar elementos a nuestra base de datos
-// si queremos ver lo que hay en post nos ayudamos de un programa externo (postman)
-users.post('/', function (req, res) {
-  res.json({
-    "mensaje":"Se creara un usuario",
-    "nombre": "javid",
-    "años" : 2026
- })
-})
+// // .post es la funcion quien nos ayuda a agregar elementos a nuestra base de datos
+// // si queremos ver lo que hay en post nos ayudamos de un programa externo (postman)
+// users.post('/', function (req, res) {
+//   res.json({
+//     "mensaje":"Se creara un usuario",
+//     "nombre": "javid",
+//     "años" : 2026
+//  })
+// })
+
+
+const userController = require('../controllers/userController');
+
+// GET: Lista todos los usuarios
+users.get('/', userController.getAllUsers);
+
+// GET: Usuario especifico por id
+users.get('/:id', userController.getUserById);
+
+// POST: Crea un usuario nuevo
+users.post('/', userController.createUser);
+
+// PUT: Actualiza un usuario existente
+users.put('/:id', userController.updateUser);
+
+// DELETE: Elimina un usuario
+users.delete('/:id', userController.deleteUser);
+
+// GET: Tareas asignadas a ese usuario
+users.get('/:id/tasks', userController.getUserTasks);
 
 //Exportamos las ruta al index.js
 module.exports = users;
